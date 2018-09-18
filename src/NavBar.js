9778ac9722bb0b1a6ea40fb27f8b1c67e75d4daf
Matt <marberrym@gmail.com>
2018-09-17 22:39:52 -0400

    Did some resizing, need to plan out the layout of the page

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 let NavBar = () =>
-    <div className="navbar"></div>
+    <div className="navbar">
+        <div className="navLink">My Goals</div>
+        <div className="flexRow">
+            <div className="navLink">Sign Up</div>
+            <div className="navLink">Log In</div>
+        </div>
+        
+        
+
+
+    </div>
 
 export default NavBar;
