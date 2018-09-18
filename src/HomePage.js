c97c74a4d08a3d7d8bc7c7f2505381b7e32a711b
Matt <marberrym@gmail.com>
2018-09-17 22:09:46 -0400

    Getting leyout started

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HeadLogo from './HeadLogo';
 import HeadCaption from './HeadCaption';
+import NavBar from './NavBar';
 
 let HomePage = (props) => 
     <div>
+        <NavBar />
         <HeadLogo />
         <HeadCaption />
     </div>
---
+++ src/NavBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+let NavBar = () =>
+    <div className="navbar"></div>
+
+export default NavBar;
