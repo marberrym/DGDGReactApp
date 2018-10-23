0b0beff454440894480a71d5e5618f6092a45cd1
Matthew <marberrym@gmail.com>
2018-10-23 18:34:24 -0400

    Have the skeleton of the database planned out.

---
+++ src/schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS dgdg_users, dgdg_goals, dgdg_likes, dgdg_likes, dgdg_comments,
+    dgdg_progress CASCADE;
+
+CREATE TABLE dgdg_users (
+    id SERIAL PRIMARY KEY,
+    username varchar(12) NOT NULL,
+    first_name varchar(200) NOT NULL,
+    last_name varchar(200) NOT NULL,
+    email varchar(200) UNIQUE NOT NULL,
+    avatar_file varchar(255),
+    password varchar(200) NOT NULL
+);
+
+CREATE TABLE dgdg_goals (
+    id SERIAL PRIMARY KEY,
+    creation_date TIMESTAMP,
+    user_id INTEGER REFERENCES dgdg_users(id),
+    goal_status INTEGER NOT NULL,
+    goal_name varchar(200) NOT NULL,
+    goal_scope INTEGER NOT NULL,
+    goal_category INTEGER NOT NULL,
+    goal_description TEXT,
+    goal_privacy INTEGER NOT NULL
+);
+
+CREATE TABLE dgdg_likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES dgdg_users(id),
+    goal_id INTEGER REFERENCES dgdg_goals(id),
+    UNIQUE (user_id, goal_id)
+);
+
+CREATE TABLE dgdg_comments (
+    id SERIAL PRIMARY KEY,
+    creation_date TIMESTAMP,
+    user_id INTEGER REFERENCES dgdg_users(id),
+    goal_id INTEGER REFERENCES dgdg_goals(id),
+    comment varchar(500) NOT NULL
+);
+
+CREATE TABLE dgdg_progress (
+    id SERIAL PRIMARY KEY,
+    goal_id INTEGER REFERENCES dgdg_goals(id),
+    progress_title varchar(500),
+    progress_description TEXT,
+    progress_image varchar(500)
+);
