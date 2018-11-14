e01c54bc2e7056e169541c0b5746ebde235fe70e
Matthew <marberrym@gmail.com>
2018-11-14 17:13:42 -0500

    working on the checkin form page with previous and next buttons for the form.

--- src/PageComponents/CheckInForm.js
+++ src/PageComponents/CheckInForm.js
@@ -5,13 +5,30 @@ let questionRouter = {
     0 : <div>
             What is a goal you have set for yourself today?
             <textarea></textarea>
-            <button className="btn">Next</button>
+        </div>,
+    1 : <div>
+            Did you do something to help yourself today?
+            <checkbox>
+                <option>Yes</option>
+                <option>Yes</option>
+            </checkbox>
         </div>
 }
 
 let CheckInForm = (props) => 
     <div>
         {questionRouter[props.question]}
+
+        {props.question > 0 ?
+            <button className="btn" onClick={event => {
+                props.update("question", props.question-1)
+            }}>Previous</button>
+        :
+            null
+        }
+        <button className="btn" onClick={event => {
+            props.update("question", props.question+1);
+        }}>Next</button>
     </div>
 
 export default CheckInForm;
