431866467146d699447c5f628c3321dbd7211efa
Matthew <marberrym@gmail.com>
2018-11-27 18:08:42 -0500

    And now my checkins are being inserted into my database correctly!  Hooray.  That's a ton of progress for today - very happy with the results.

--- routes/newCheckin.js
+++ routes/newCheckin.js
@@ -3,7 +3,16 @@ const jwt = require('jsonwebtoken');
 
 let newCheckin = (req, res) => {
     let decoded = jwt.decode(req.headers.token);
-    console.log(req.body);
+    db.none(`INSERT into dgdg_checkins (creation_date, user_id, goal, goal_outcome, 
+        goal_improvements, self_help, self_help_response, help_others, help_others_response) VALUES 
+        (CURRENT_TIMESTAMP, $1, $2, $3, $4, $5, $6, $7, $8)`, [decoded.id, req.body.goal,
+        req.body.goal_outcome, req.body.goal_improvements, req.body.self_help, req.body.self_help_response,
+        req.body.help_others, req.body.help_others_response])
+    .then(res.send({status: "success"}))
+    .catch(err => {
+        console.log(err);
+        res.send({status: "error"});
+    })
 }
 
 module.exports = newCheckin;
